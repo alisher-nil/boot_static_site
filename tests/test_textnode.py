229e441bfdd4dc0e8cd0259b14d56e5c4dd0b371
Alisher Nildibayev <alisher.nil@gmail.com>
2025-10-08 23:07:37 +0500

    Fix test parameterization for TextNode types and values in TestTextNode class

--- tests/test_textnode.py
+++ tests/test_textnode.py
@@ -8,11 +8,23 @@ class TestTextNode:
         "type1, type2",
         [
             [TextType.BOLD, TextType.CODE],
-            [TextType.BOLD, TextType.TEXT],
+            [TextType.ITALIC, TextType.TEXT],
         ],
     )
     def test_different_types(self, type1, type2):
         common_text = "same text for both"
         node1 = TextNode(common_text, type1)
         node2 = TextNode(common_text, type2)
+        assert node1 != node2
+
+    @pytest.mark.parametrize(
+        "value1, value2",
+        [
+            ["text1", "text2"],
+            ["same same", "but defferent"],
+        ],
+    )
+    def test_different_values(self, value1, value2):
+        node1 = TextNode(value1, TextType.TEXT)
+        node2 = TextNode(value2, TextType.TEXT)
         assert node1 != node2
