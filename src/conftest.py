7e46e40193d6394d8d4a2ca3d4a964536384455b
Alisher Nildibayev <alisher.nil@gmail.com>
2025-11-30 00:31:05 +0500

    Add pytest fixtures and tests for TextNode functionality

---
+++ src/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+
+from textnode import TextNode, TextType
+
+
+@pytest.fixture
+def bold_text_params():
+    return {"text": "This is a text node", "text_type": TextType.BOLD}
+
+
+@pytest.fixture
+def italic_text_params():
+    return {"text": "This is a text node", "text_type": TextType.ITALIC}
+
+
+@pytest.fixture
+def bold_text_node(bold_text_params):
+    return TextNode(**bold_text_params)
+
+
+@pytest.fixture
+def italic_text_node(italic_text_params):
+    return TextNode(**italic_text_params)
+
+
+@pytest.fixture
+def same_bold_text_node(bold_text_params):
+    return TextNode(**bold_text_params)
+
+
+@pytest.fixture
+def different_bold_text_node():
+    return TextNode("completely different text", TextType.BOLD)
---
+++ src/test_textnode.py
@@ -0,0 +1,30 @@
+import pytest
+from pytest_lazy_fixtures import lf
+
+
+class TestTextNode:
+    @pytest.mark.parametrize(
+        "node1, node2, expected_result",
+        [
+            (lf("bold_text_node"), lf("same_bold_text_node"), True),
+            (lf("bold_text_node"), lf("different_bold_text_node"), False),
+            (lf("bold_text_node"), lf("italic_text_node"), False),
+        ],
+    )
+    def test_eq(self, node1, node2, expected_result):
+        assert (node1 == node2) == expected_result
+
+    @pytest.mark.parametrize(
+        "node, params",
+        (
+            (lf("bold_text_node"), lf("bold_text_params")),
+            (lf("italic_text_node"), lf("italic_text_params")),
+        ),
+    )
+    def test_str(self, node, params):
+        text = params.get("text")
+        text_type = params.get("text_type")
+        url = params.get("url")
+
+        expected_string = f"TextNode({text}, {text_type.value}, {url})"
+        assert str(node) == expected_string
