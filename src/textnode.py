fe2328e2a497a9a0a565b9d0df0471237f4962c9
Alisher Nildibayev <alisher.nil@gmail.com>
2025-12-05 18:06:40 +0500

    feat: implement text_node_to_html_node function for converting TextNode to LeafNode

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
 
 class TextType(Enum):
     TEXT = "text"
@@ -27,4 +29,23 @@ class TextNode:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
 
-def text_node_to_html_node(text_node: TextNode): ...
+def text_node_to_html_node(text_node: TextNode):
+    if text_node.text_type not in TextType:
+        raise ValueError(f"Invalid text type: {text_node.text_type}")
+
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        if text_node.url is None:
+            raise ValueError("Link text node url can not be empty")
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        if text_node.url is None:
+            raise ValueError("Image text node url can not be empty")
+        return LeafNode("img", "", {"alt": text_node.text, "src": text_node.url})
