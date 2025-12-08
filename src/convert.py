6e7e3beef3501c2a74eae83c539a998143f8cf32
Alisher Nildibayev <alisher.nil@gmail.com>
2025-12-08 15:34:13 +0500

    feat: implement split_nodes_delimiter function for processing node splits

--- src/convert.py
+++ src/convert.py
@@ -1,10 +1,31 @@
 from typing import Sequence
 
-from textnode import TextNode, TextType
+from .textnode import TextNode, TextType
 
 
 def split_nodes_delimiter(
     old_nodes: Sequence[TextNode],
     delimiter: str,
     text_type: TextType,
-) -> Sequence[TextNode]: ...
+) -> Sequence[TextNode]:
+    result = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            result.append(node)
+            continue
+        new_nodes = []
+        parts = node.text.split(delimiter)
+        if len(parts) % 2 == 0:
+            raise ValueError("invalid markdown syntax")
+
+        for i in range(len(parts)):
+            part = parts[i]
+            if not part:
+                continue
+            if i % 2 == 0:
+                new_nodes.append(TextNode(part, TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(part, text_type))
+
+        result.extend(new_nodes)
+    return result
