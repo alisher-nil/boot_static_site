5b6b1909458e085bb56bffc2b8e738ffeb3654b3
Alisher Nildibayev <alisher.nil@gmail.com>
2025-12-08 15:34:31 +0500

    test: add unit tests for split_nodes_delimiter function

--- tests/textnode/test_textnode.py
+++ tests/textnode/test_textnode.py
@@ -1,6 +1,7 @@
 import pytest
 from pytest_lazy_fixtures import lf
 
+from src.convert import split_nodes_delimiter
 from src.textnode import TextNode, TextType
 
 
@@ -34,3 +35,27 @@ class TestTextNode:
 
         expected_string = f"TextNode({text}, {text_type.value}, {url})"
         assert str(node) == expected_string
+
+
+class TestSplitNodes:
+    def test_convert_function(self):
+        node = TextNode(
+            "`def main():...` a simple example `return None`",
+            text_type=TextType.TEXT,
+        )
+        result = split_nodes_delimiter([node], "`", TextType.CODE)
+        assert len(result) == 3
+
+    def test_empty_list(self):
+        nodes = split_nodes_delimiter([], "~", TextType.BOLD)
+        assert len(nodes) == 0
+
+    def test_empty_string(self):
+        node = TextNode("", TextType.TEXT)
+        nodes = split_nodes_delimiter([node], "*", TextType.ITALIC)
+        assert len(nodes) == 0
+
+    def test_invalid_string(self):
+        node = TextNode("bad code` no opening backtick", TextType.TEXT)
+        with pytest.raises(ValueError, match="invalid markdown syntax"):
+            split_nodes_delimiter([node], "`", TextType.CODE)
