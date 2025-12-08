d5e5d37643084d551feaf632ab83bf320702b542
Alisher Nildibayev <alisher.nil@gmail.com>
2025-12-08 22:40:55 +0500

    test: add tests for extract_markdown_images and extract_markdown_links functions

--- tests/textnode/test_markdown.py
+++ tests/textnode/test_markdown.py
@@ -1,6 +1,10 @@
 import pytest
 
-from src.markdown import split_nodes_delimiter
+from src.markdown import (
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_delimiter,
+)
 from src.textnode import TextNode, TextType
 
 
@@ -36,3 +40,55 @@ class TestSplitNodes:
             TextNode(" and ", TextType.TEXT),
             TextNode("italic", TextType.ITALIC),
         ]
+
+
+class TestExtractMarkdown:
+    @pytest.mark.parametrize(
+        "text, expected_matches",
+        [
+            (
+                "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)",
+                [("image", "https://i.imgur.com/zjjcJKZ.png")],
+            ),
+            (
+                "This is text with link and no image [image](https://i.imgur.com/zjjcJKZ.png)",
+                [],
+            ),
+            (
+                "This is text with two images ![image](https://i.imgur.com/zjjcJKZ.png), "
+                "![another_image](https://i.imgur.com/zjjcTRU.png)",
+                [
+                    ("image", "https://i.imgur.com/zjjcJKZ.png"),
+                    ("another_image", "https://i.imgur.com/zjjcTRU.png"),
+                ],
+            ),
+        ],
+    )
+    def test_extract_markdown_images(self, text, expected_matches):
+        matches = extract_markdown_images(text)
+        assert matches == expected_matches
+
+    @pytest.mark.parametrize(
+        "text, expected_matches",
+        [
+            (
+                "This is text with a link [shitsumon](https://www.example.com)",
+                [("shitsumon", "https://www.example.com")],
+            ),
+            (
+                "This is text with image ![image](https://i.imgur.com/zjjcJKZ.png)",
+                [],
+            ),
+            (
+                "This is text with two links [shitsumon](https://www.example.com), "
+                "[mysaly](https://www.example.net)",
+                [
+                    ("shitsumon", "https://www.example.com"),
+                    ("mysaly", "https://www.example.net"),
+                ],
+            ),
+        ],
+    )
+    def test_extract_markdown_links(self, text, expected_matches):
+        matches = extract_markdown_links(text)
+        assert matches == expected_matches
