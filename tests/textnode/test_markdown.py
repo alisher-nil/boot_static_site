e19bac99f629a639167453f927557cab85d0266d
Alisher Nildibayev <alisher.nil@gmail.com>
2025-12-11 18:18:59 +0500

    test: update test_split_images for improved readability of input string

--- tests/textnode/test_markdown.py
+++ tests/textnode/test_markdown.py
@@ -96,7 +96,8 @@ class TestExtractMarkdown:
 
     def test_split_images(self):
         node = TextNode(
-            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another "
+            "![second image](https://i.imgur.com/3elNhQu.png)",
             TextType.TEXT,
         )
         new_nodes = split_nodes_image([node])
