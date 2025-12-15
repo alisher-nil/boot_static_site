13d1994488c87366f9fdcba8e16f08362b1257f2
Alisher Nildibayev <alisher.nil@gmail.com>
2025-12-15 19:14:32 +0500

    feat: add BlockType enum and block_to_block_type function for markdown processing

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,4 +1,25 @@
+import re
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     block_splitter = "\n\n"
     blocks = [block.strip() for block in markdown.split(block_splitter) if block.strip()]
     return blocks
+
+
+def block_to_block_type(markdown: str) -> BlockType:
+    heading_pattern = r"^[#]{1,6} .*$"
+    if re.match(heading_pattern, markdown):
+        return BlockType.HEADING
+    else:
+        return BlockType.PARAGRAPH
