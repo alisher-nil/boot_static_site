a305e527ff044f85dc39e9105d4331b7e9c168fa
Alisher Nildibayev <alisher.nil@gmail.com>
2026-01-02 03:05:09 +0500

    feat: restructure project by adding new CSS and HTML files, removing old ones, and implementing directory management in main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,27 @@
+import os
+import shutil
+
+from src.constants import PUBLIC_DIR, STATIC_DIR
+
+
 def main():
-    print("hello")
+    reset_public_directory()
+
+
+def reset_public_directory():
+    if os.path.exists(PUBLIC_DIR):
+        shutil.rmtree(PUBLIC_DIR)
+    os.mkdir(PUBLIC_DIR)
+    copy_files(STATIC_DIR, PUBLIC_DIR)
+
+
+def copy_files(src, dst):
+    for item in os.listdir(src):
+        if os.path.isfile(os.path.join(src, item)):
+            shutil.copy(os.path.join(src, item), dst)
+        else:
+            os.mkdir(os.path.join(dst, item))
+            copy_files(os.path.join(src, item), os.path.join(dst, item))
 
 
 if __name__ == "__main__":
